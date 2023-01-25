83485f3d62ca1ffbeb35260797df857f226d5c32
Re-Programmed <reprogrammed.yt@gmail.com>
2023-01-24 20:44:04 -0700

    Added prereq.

---
+++ Finnss/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finnss
+{
+    public class Person
+    {
+        readonly string firstName;
+        readonly string lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            return $"{lastName}, {firstName}";
+        }
+    }
+}
--- Finnss/Program.cs
+++ Finnss/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finnss
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Program start code
+            Person p = new Person("Bobbi", "Jo");
+
+            printPerson(p);
+        }
+
+        static void printPerson(Person person)
+        {
+            Console.WriteLine(person.GetFullName());
+        }
+    }
+}
